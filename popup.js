8408df76cc9b6a963c88070719860946c13b8c93
Matthew Violet <mattviolet123@gmail.com>
2018-09-07 11:56:14 -0400

    deleted duplicate registration button

--- popup.js
+++ popup.js
@@ -65,33 +65,9 @@ registerButton.addEventListener('click', function() {
     });
   document.getElementById("login-button").disabled = true;
   document.getElementById("login-done").style.display = "block";
-    
+
 }, false);
 
-registerButton.addEventListener('click', function() {
-    var email = document.getElementById('email').value;
-    var password = document.getElementById('password').value;
-
-    firebase.auth().sendSignInLinkToEmail(email, actionCodeSettings)
-    .then(function() {
-        // The link was successfully sent. Inform the user.
-        alert("A link was sent to your email for verification.")
-        // Save the email locally so you don't need to ask the user for it again
-        // if they open the link on the same device.
-        window.localStorage.setItem('emailForSignIn', email);
-        firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
-            // Handle Errors here.
-            var errorCode = error.code;
-            var errorMessage = error.message;
-
-            alert(errorMessage);
-        });
-    })
-    .catch(function(error) {
-        // Some error occurred, you can inspect the code: error.code
-        alert(error.message)
-    });
-}, false);
 
 var actionCodeSettings = {
     // URL you want to redirect back to. The domain (www.example.com) for this
