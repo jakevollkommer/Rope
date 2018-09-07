27fc57d91d8a5cd38954b155469c3f2d2d85526f
Ridoy Majumdar <majumdar.ridoy@gmail.com>
2018-09-07 11:41:29 -0400

    Login implementation

--- popup.js
+++ popup.js
@@ -1,39 +1,71 @@
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyDEuPZLh_r7FzRt5PACRuGnRmaXmNacxdk",
-    authDomain: "rope-b4dbf.firebaseapp.com",
-    databaseURL: "https://rope-b4dbf.firebaseio.com",
-    projectId: "rope-b4dbf",
-    storageBucket: "rope-b4dbf.appspot.com",
-    messagingSenderId: "794052295835"
-  };
-  firebase.initializeApp(config);
+// Initialize Firebase
+var config = {
+	apiKey: "AIzaSyDEuPZLh_r7FzRt5PACRuGnRmaXmNacxdk",
+	authDomain: "rope-b4dbf.firebaseapp.com",
+	databaseURL: "https://rope-b4dbf.firebaseio.com",
+	projectId: "rope-b4dbf",
+	storageBucket: "rope-b4dbf.appspot.com",
+	messagingSenderId: "794052295835"
+};
+firebase.initializeApp(config);
 
 
 var loginButton = document.getElementById('login-button');
 var registerButton = document.getElementById('register-button');
-
+firebase.auth().onAuthStateChanged(function(user) {
+	if (user) {
+		console.log(user);
+		// ...
+	} else {
+		// User is signed out.
+		// ...
+	}
+});
 
 loginButton.addEventListener('click', function() {
-  document.getElementById("login-button").disabled = true;
-  document.getElementById("login-done").style.display = "block";
-    
+	document.getElementById("login-button").disabled = true;
+	document.getElementById("login-done").style.display = "block";
+	var email = document.getElementById('email').value;
+	var password = document.getElementById('password').value;
+	firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+		var errorCode = error.code;
+		var errorMessage = error.message;
+		console.log(errorCode, errorMessage);
+	});
+
 }, false);
 
 registerButton.addEventListener('click', function() {
-	var email = document.getElementById('email').value;
-	var password = document.getElementById('password').value;
-  
-  document.getElementById("register-button").disabled = true;
-  document.getElementById("register-done").style.display = "block";
-  
-  firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
-	  // Handle Errors here.
-	  var errorCode = error.code;
-	  var errorMessage = error.message;
+    var email = document.getElementById('email').value;
+    var password = document.getElementById('password').value;
 
-	  console.log(errorCode);
-	  console.log(errorMessage);
-	  // ...
-	});
+
+    firebase.auth().sendSignInLinkToEmail(email, actionCodeSettings)
+    .then(function() {
+        // The link was successfully sent. Inform the user.
+        alert("A link was sent to your email for verification.")
+        // Save the email locally so you don't need to ask the user for it again
+        // if they open the link on the same device.
+        window.localStorage.setItem('emailForSignIn', email);
+        firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+
+            alert(errorMessage);
+        });
+    })
+    .catch(function(error) {
+        // Some error occurred, you can inspect the code: error.code
+        alert(error.message)
+    });
 }, false);
+
+var actionCodeSettings = {
+    // URL you want to redirect back to. The domain (www.example.com) for this
+    // URL must be whitelisted in the Firebase Console.
+    url: 'https://www.twine.org',
+    // This must be true.
+    handleCodeInApp: true,
+};
+
