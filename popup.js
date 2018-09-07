ea19c12ab3d61571770b85550e328dca37fe89f1
jakevollkommer <jakexxvollkommer@aol.com>
2018-09-07 11:15:35 -0400

    verification email is sent to email upon registration

--- popup.js
+++ popup.js
@@ -13,21 +13,52 @@
 var loginButton = document.getElementById('login-button');
 var registerButton = document.getElementById('register-button');
 
-
 loginButton.addEventListener('click', function() {
+  var email = document.getElementById('email').value;
+  var password = document.getElementById('password').value;
+
+  firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+
+      alert(errorMessage);
+  });
+  alert("login successful")
+
 }, false);
 
 registerButton.addEventListener('click', function() {
-	var email = document.getElementById('email').value;
-	var password = document.getElementById('password').value;
+    var email = document.getElementById('email').value;
+    var password = document.getElementById('password').value;
 
-	firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
-	  // Handle Errors here.
-	  var errorCode = error.code;
-	  var errorMessage = error.message;
 
-	  console.log(errorCode);
-	  console.log(errorMessage);
-	  // ...
-	});
+    firebase.auth().sendSignInLinkToEmail(email, actionCodeSettings)
+    .then(function() {
+        // The link was successfully sent. Inform the user.
+        alert("A link was sent to your email for verification.")
+        // Save the email locally so you don't need to ask the user for it again
+        // if they open the link on the same device.
+        window.localStorage.setItem('emailForSignIn', email);
+        firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+
+            alert(errorMessage);
+        });
+    })
+    .catch(function(error) {
+        // Some error occurred, you can inspect the code: error.code
+        alert(error.message)
+    });
 }, false);
+
+var actionCodeSettings = {
+    // URL you want to redirect back to. The domain (www.example.com) for this
+    // URL must be whitelisted in the Firebase Console.
+    url: 'https://www.twine.org',
+    // This must be true.
+    handleCodeInApp: true,
+};
+
