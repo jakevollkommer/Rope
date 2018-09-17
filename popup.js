cbc77d1a2aec2c8ccf105e36f6a8d8fb87f82e12
jakevollkommer <jakexxvollkommer@aol.com>
2018-09-17 00:28:59 -0400

    user is redirected to twine upon login. False successful login bug fixed

--- popup.js
+++ popup.js
@@ -1,47 +1,50 @@
 // Initialize Firebase
 var config = {
-	apiKey: "AIzaSyDEuPZLh_r7FzRt5PACRuGnRmaXmNacxdk",
-	authDomain: "rope-b4dbf.firebaseapp.com",
-	databaseURL: "https://rope-b4dbf.firebaseio.com",
-	projectId: "rope-b4dbf",
-	storageBucket: "rope-b4dbf.appspot.com",
-	messagingSenderId: "794052295835"
+    apiKey: "AIzaSyDEuPZLh_r7FzRt5PACRuGnRmaXmNacxdk",
+    authDomain: "rope-b4dbf.firebaseapp.com",
+    databaseURL: "https://rope-b4dbf.firebaseio.com",
+    projectId: "rope-b4dbf",
+    storageBucket: "rope-b4dbf.appspot.com",
+    messagingSenderId: "794052295835"
 };
 firebase.initializeApp(config);
 
 var loginButton = document.getElementById('login-button');
 var registerButton = document.getElementById('register-button');
 firebase.auth().onAuthStateChanged(function(user) {
-	if (user) {
-		console.log(user);
-		// ...
-	} else {
-		// User is signed out.
-		// ...
-	}
+    if (user) {
+        console.log(user);
+        // ...
+    } else {
+        // User is signed out.
+        // ...
+    }
 });
 
 loginButton.addEventListener('click', function() {
-	document.getElementById("login-button").disabled = true;
-	document.getElementById("login-done").style.display = "block";
-  var email = document.getElementById('email').value;
-  var password = document.getElementById('password').value;
+    var email = document.getElementById('email').value;
+    var password = document.getElementById('password').value;
 
-  firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
-      // Handle Errors here.
-      var errorCode = error.code;
-      var errorMessage = error.message;
+    firebase.auth().signInWithEmailAndPassword(email, password).then(function(user) {
+        document.getElementById("login-button").disabled = true;
+        document.getElementById("login-done").style.display = "block";
 
-      alert(errorMessage);
-  });
-  alert("login successful")
+        chrome.tabs.update({
+            url: "http://twinery.org/2/#!/stories"
+        });
+    }).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+
+        alert(errorMessage);
+    });
 
 }, false);
 
 registerButton.addEventListener('click', function() {
     var email = document.getElementById('email').value;
     var password = document.getElementById('password').value;
-
 
     firebase.auth().sendSignInLinkToEmail(email, actionCodeSettings)
     .then(function() {
@@ -62,17 +65,7 @@ registerButton.addEventListener('click', function() {
         // Some error occurred, you can inspect the code: error.code
         alert(error.message)
     });
-  document.getElementById("login-button").disabled = true;
-  document.getElementById("login-done").style.display = "block";
+    document.getElementById("login-button").disabled = true;
+    document.getElementById("login-done").style.display = "block";
 
 }, false);
-
-
-var actionCodeSettings = {
-    // URL you want to redirect back to. The domain (www.example.com) for this
-    // URL must be whitelisted in the Firebase Console.
-    url: 'https://www.twine.org',
-    // This must be true.
-    handleCodeInApp: true,
-};
-
