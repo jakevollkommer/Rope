28880292ab9ae071399b0aca68eba3fac61ef3f1
Matthew Violet <mattviolet123@gmail.com>
2018-09-24 22:39:09 -0400

    Fixed login and logout screens and register screens

--- popup.js
+++ popup.js
@@ -11,13 +11,18 @@ firebase.initializeApp(config);
 
 var loginButton = document.getElementById('login-button');
 var registerButton = document.getElementById('register-button');
+var logoutButton = document.getElementById('logout-button');
+
 firebase.auth().onAuthStateChanged(function(user) {
     if (user) {
         console.log(user);
-        // ...
+        document.getElementById("LoginPage").style.display='none';
+        document.getElementById("HomePage").style.display='block';
     } else {
         // User is signed out.
         // ...
+        document.getElementById("LoginPage").style.display='block';
+        document.getElementById("HomePage").style.display='none';
     }
 });
 
@@ -26,12 +31,28 @@ loginButton.addEventListener('click', function() {
     var password = document.getElementById('password').value;
 
     firebase.auth().signInWithEmailAndPassword(email, password).then(function(user) {
-        document.getElementById("login-button").disabled = true;
-        document.getElementById("login-done").style.display = "block";
-
         chrome.tabs.update({
             url: "http://twinery.org/2/#!/stories"
         });
+    }).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+
+        alert(errorMessage);
+    });
+
+}, false);
+
+
+logoutButton.addEventListener('click', function() {
+    firebase.auth().signOut().then(function(user) {
+        console.log("logged out");
+
+        chrome.tabs.update({
+            url: "http://google.com"
+        });
+
     }).catch(function(error) {
         // Handle Errors here.
         var errorCode = error.code;
@@ -49,24 +70,29 @@ registerButton.addEventListener('click', function() {
     firebase.auth().sendSignInLinkToEmail(email, actionCodeSettings)
     .then(function() {
         // The link was successfully sent. Inform the user.
-        alert("A link was sent to your email for verification.")
         // Save the email locally so you don't need to ask the user for it again
         // if they open the link on the same device.
         window.localStorage.setItem('emailForSignIn', email);
-        firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+        firebase.auth().createUserWithEmailAndPassword(email, password).then(function() {
+            document.getElementById("register-button").disabled = true;
+            document.getElementById("register-done").style.display = "block";
+
+            alert("A link was sent to your email for verification.")
+        }).catch(function(error) {
             // Handle Errors here.
             var errorCode = error.code;
             var errorMessage = error.message;
+            alert(error.message);
 
-            alert(errorMessage);
         });
+
+
     })
     .catch(function(error) {
         // Some error occurred, you can inspect the code: error.code
-        alert(error.message)
+        alert(error.message);
     });
-    document.getElementById("login-button").disabled = true;
-    document.getElementById("login-done").style.display = "block";
+
 
 }, false);
 var actionCodeSettings = {
