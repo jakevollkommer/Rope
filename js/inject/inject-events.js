7f34cd677780432d14cd240d10640917eb7e44cc
Gautum <gautum.suresh13@gmail.com>
2018-10-17 16:08:50 -0400

    got a share button to pop up

--- js/inject/inject-events.js
+++ js/inject/inject-events.js
@@ -1,6 +1,12 @@
 ContentScript.prototype.addListeners = function() {
     window.onhashchange = function(e) {
         var sharing = true;
-        
+
+        if (sharing) {
+            var button = document.createElement("button")
+            button.innerText = "Share"
+            var body = document.getElementById("storyEditView");
+            body.insertBefore(button, t.childNodes[0])
+        }
     }
 }
