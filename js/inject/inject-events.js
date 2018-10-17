ccacc2c599c3860777785a6ba399efd16c2aa0e9
tsun1650 <tonysun996@gmail.com>
2018-10-17 17:48:59 -0400

    half of modal NOT FUNCTIONAL

--- js/inject/inject-events.js
+++ js/inject/inject-events.js
@@ -1,12 +1,31 @@
+var modal = document.getElementById('myModal');
+var span = document.getElementsByClassName("close")[0];
+        
 ContentScript.prototype.addListeners = function() {
     window.onhashchange = function(e) {
-        var sharing = true;
+        console.log("window has changed")
+        var share_button = document.createElement("button")
+        share_button.innerText = "Share"
+        var body = document.getElementById("storyEditView");
+        var l = body.lastElementChild.firstElementChild;
+        l.appendChild(share_button);
 
-        if (sharing) {
-            var button = document.createElement("button")
-            button.innerText = "Share"
-            var body = document.getElementById("storyEditView");
-            body.insertBefore(button, t.childNodes[0])
-        }
+        
+        share_button.addEventListener("click", function(){
+
+            console.log("clicked");
+            modal.style.display = "block";
+            span.onclick = function() {
+                modal.style.display = "none";
+            }
+            window.onclick = function(event) {
+                if (event.target == modal) {
+                    modal.style.display = "none";
+                }
+            }
+            var sub_email = document.getElementById('emailfield').value;
+            console.log(sub_email);
+        });
+
     }
 }
