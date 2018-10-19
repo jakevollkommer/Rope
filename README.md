d699062cdcf50a6a2b4bfb1f75648f77b8b2e30c
jakevollkommer <jakexxvollkommer@aol.com>
2018-10-19 12:46:44 -0400

    added gulp watch

--- README.md
+++ README.md
@@ -5,7 +5,8 @@ An extension to Twine which allows users to collaborate in real time.
 
 1. Have node.js installed
 2. Clone repo
-3. `npm install` 
+3. `npm install`
 4. Make sure that gulp version 3.9.1 is installed. (npm install 3.9.1 --save)
 5. `gulp`
-6. Add to chrome://extensions
+6. `npm install --save-dev gulp-watch`
+7. Add to chrome://extensions
--- js/background/background-sync.js
+++ js/background/background-sync.js
@@ -7,45 +7,45 @@
  * @param userId the signed-in user's uid
  */
 Background.prototype.getUserStoriesFromFirebase = function(userId) {
-	var $this = this;
-	let stories = [];
-	let passages = [];
-	return new Promise(function(resolve, reject) {
-		// 1. Get ids of user's stories
-		$this.database.ref('userStories/' + userId).once('value')
-			.then(function(snapshot) {
-				const userStoriesIds = snapshot.val();
+    var $this = this;
+    let stories = [];
+    let passages = [];
+    return new Promise(function(resolve, reject) {
+        // 1. Get ids of user's stories
+        $this.database.ref('userStories/' + userId).once('value')
+            .then(function(snapshot) {
+                const userStoriesIds = snapshot.val();
                 if (!userStoriesIds) {
                     return reject('user has no stories');
                 }
-				const storiesQuery = userStoriesIds.map(id => {
-					return $this.database.ref('stories/' + id).once('value')
-						.then(s => stories.push(s.val()));
-				})
-				const passagesQuery = userStoriesIds.map(id => {
-					return $this.database.ref('passages').orderByChild('story')
-						.equalTo(id).once('value').then(s => {
-							Object.keys(s.val()).map(key => {
-								passages.push(s.val()[key]);
-							});
-						});
-				});
-				// 2. Get this user's stories
-				Promise.all(storiesQuery)
-					.then(storiesInDB => {
-						// 3. Get the passages of those stories
-						return Promise.all(passagesQuery);
-					})
-					.then(passagesInDB =>{
-						// 4. Return to database
-						resolve({
-							stories: stories,
-							passages: passages
-						});
-					})
-					.catch(err => reject(err))
-			});
-	});
+                const storiesQuery = userStoriesIds.map(id => {
+                    return $this.database.ref('stories/' + id).once('value')
+                        .then(s => stories.push(s.val()));
+                })
+                const passagesQuery = userStoriesIds.map(id => {
+                    return $this.database.ref('passages').orderByChild('story')
+                        .equalTo(id).once('value').then(s => {
+                            Object.keys(s.val()).map(key => {
+                                passages.push(s.val()[key]);
+                            });
+                        });
+                });
+                // 2. Get this user's stories
+                Promise.all(storiesQuery)
+                    .then(storiesInDB => {
+                        // 3. Get the passages of those stories
+                        return Promise.all(passagesQuery);
+                    })
+                    .then(passagesInDB =>{
+                        // 4. Return to database
+                        resolve({
+                            stories: stories,
+                            passages: passages
+                        });
+                    })
+                    .catch(err => reject(err))
+            });
+    });
 }
 
 Background.prototype.addUsersToStory = function(userEmails) {
