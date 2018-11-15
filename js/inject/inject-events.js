e75336271907fcd59edede6ee2174d6fd4a9aa60
tsun1650 <tonysun996@gmail.com>
2018-11-14 21:11:17 -0500

    multiple collabs add

--- js/inject/inject-events.js
+++ js/inject/inject-events.js
@@ -1,66 +1,53 @@
 ContentScript.prototype.addListeners = function() {
     var $this = this;
     window.onhashchange = function(e) {
-        console.log("window has changed")
-        var share_button = document.createElement("button")
-        share_button.setAttribute("class", "share-button");
-        share_button.innerText = "Share"
+
+        /*Inject share button on toolbar */
+        var shareBtn = document.createElement("button")
+        shareBtn.setAttribute("class", "share-button");
+        shareBtn.innerText = "Share"
         var body = document.getElementById("storyEditView");
         var l = body.lastElementChild.firstElementChild;
-        l.appendChild(share_button);
+        l.appendChild(shareBtn);
 
-        // createPopup();
+        /*Inject share with others popup on screen */
         var xmlHttp = null;
-
         xmlHttp = new XMLHttpRequest();
         xmlHttp.open( "GET", chrome.extension.getURL ("html/sharePopup.html"), false );
         xmlHttp.send( null );
-
         var inject  = document.createElement("div");
         inject.innerHTML = xmlHttp.responseText
-        document.body.insertBefore (inject, document.body.firstChild);
+        document.body.insertBefore(inject, document.body.firstChild);
+        /* Go ahead and define all the buttons*/
+        var input = document.getElementById("inputSubmit");
+        var submitBtn = document.getElementById("btnSubmit");
+        var closeBtn = document.getElementById("closeButton");
+        var pop = document.getElementById("myPopup");
 
-        // document.getElementById("myPopup").visibility = 'visible';
-        // var popup = $this.createPopup();
-        // popup.setAttribute("class", "modal");
-        // body.insertBefore(popup,body.childNodes[1]);
-
-        share_button.addEventListener("click", function(){
-            var blur_div = document.createElement('div');
-            blur_div.setAttribute("class", "blur-effect");
-            body.appendChild(blur_div);
-            var pop = document.getElementById("myPopup")
-            pop.style.visibility = 'visible';
-        });
-        var btnSubmit = document.getElementById("btnSubmit");
-
-        btnSubmit.addEventListener("click", function() {
-            var input = document.getElementById("inputSubmit");
+        /*Button listeners*/
+        submitBtn.addEventListener("click", function() { 
             var sharedEmail = input.value;
+            input.value = "";
             if (sharedEmail) {
-                // Hide popup
-                var popup = document.getElementById("myPopup");
-                popup.style.visibility = 'hidden';
-                var img = document.createElement("IMG");
-                var imgURL = chrome.extension.getURL('img/cloud.png');
-                img.src = imgURL;
-                var body = document.getElementById("storyEditView");
-                var l = body.lastElementChild.firstElementChild;
-                $this.sendMessage($this.buildAddUsersRequest(sharedEmail, storyId));
+                
+                pop.style.visibility = 'hidden';
+                
+                var multipleEmails = sharedEmail.split(",");
+                for (i = 0; i < multipleEmails.length; i++) {
+                    $this.sendMessage($this.buildAddUsersRequest(multipleEmails[i].replace(/\s+/g, ''), storyId));
+                }
                 $this.sendMessage($this.buildUploadStoryRequest(storyId));
+
+                var img = document.createElement("IMG");
+                img.src = chrome.extension.getURL('img/cloud.png');
+                shareBtn.appendChild(img);
             }
         });
-
-        var closeBtn = document.getElementById("closeButton");
+        
         closeBtn.addEventListener("click", function() {
-
-            var pop = document.getElementById("myPopup");
-            pop.style.visibility = 'hidden';
-            console.log("closeing");
-
-        })
-
-
+            pop.style.visibility = 'hidden'; 
+            input.value = "";
+        });
     }
 };
 
@@ -96,72 +83,4 @@ ContentScript.prototype.buildAddUsersRequest = function(sharedEmail, storyId) {
         storyId: storyId
     };
     return addUsersRequest;
-}
-
-ContentScript.prototype.createPopup = function() {
-    let $this = this;
-    popup = document.createElement('div');
-    // popup.src = chrome.extension.getURL('html/myModal.html')
-    //popup.position = absolute;
-    // popup.innerText = 'Share';
-
-    popup.id = 'myPopup';
-
-    // var input = document.createElement('input');
-    var label = document.createElement('label');
-    label.innerText = 'Enter email to share.'
-    label.style.color = 'black';
-
-    var input = document.createElement('input');
-    input.setAttribute('type','text');
-    input.setAttribute('class','modal-input');
-
-    var close_button = document.createElement('button');
-    close_button.setAttribute('class','modal-button--close');
-    close_button.innerText = "Close";
-
-    close_button.addEventListener("click",function() {
-        console.log("tried to close");
-        popup.style.visibility = 'hidden';
-    });
-
-
-    var btnsubmit = document.createElement('button');
-    btnsubmit.setAttribute("type", "submit");
-    btnsubmit.setAttribute("class", "modal-button--submit");
-    btnsubmit.innerText = "Submit";
-
-    let storyId = window.location.hash.split("/").pop()
-
-    btnsubmit.addEventListener("click", function() {
-        var sharedEmail = input.value;
-        if (sharedEmail) {
-            // Hide popup
-            popup.style.visibility = 'hidden';
-            var img = document.createElement("IMG");
-            var imgURL = chrome.extension.getURL('img/cloud.png');
-            img.src = imgURL;
-            var body = document.getElementById("storyEditView");
-            var l = body.lastElementChild.firstElementChild;
-            $this.sendMessage($this.buildAddUsersRequest(sharedEmail, storyId));
-            $this.sendMessage($this.buildUploadStoryRequest(storyId));
-        }
-    });
-
-
-    popup.appendChild(label);
-    popup.appendChild(input);
-
-
-    var sub_div = document.createElement('div');
-    sub_div.appendChild(btnsubmit);
-    sub_div.appendChild(close_button);
-
-    // popup.appendChild(btnsubmit);
-    // popup.appendChild(close_button);
-
-    popup.appendChild(sub_div);
-    opened = true;
-    return popup;
-    // share_button.appendChild(popup);
 }
